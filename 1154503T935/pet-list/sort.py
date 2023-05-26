887f476bc9de15f5d9d1f29bbb349d876434878c
Jayther2020 <dominoxxx@foxmail.com>
2023-05-26 22:38:03 +0800

    refresh

--- 1154503T935/pet-list/sort.py
+++ 1154503T935/pet-list/sort.py
@@ -7,12 +7,23 @@ for file in os.listdir():
     if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".gif"):
         image_files.append(file)
 
+with open('names.txt','r',encoding='utf-8') as f:
+    names = f.read().split(' ')
+
+image_map = list(zip(names,image_files))
+print(image_map)
+
 result = list(map(lambda f:{
-    "name":'测试花瓶',
+    "name":f[0],
     "isNew":False,
+    "isOriginal":False,
+    "hasOtherForm":False,
+    "hasOtherConcept":False,
     'type':None,
-    'picUrl':f,
-    }, image_files))
+    'picUrl':f[1],
+    }, image_map))
+
+print(result[0])
 
 with open('rocodex.json','w') as f:
     f.write(json.dumps(result))
